916fb0623391f4419aeb265e9b607b15b8760bc4
moisesdavila2005-max <moisesdavila2005@gmail.com>
2026-06-18 11:11:59 -0600

    Actualizar layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -2,7 +2,7 @@ import WhatsAppFloat from './components/WhatsAppFloat'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang= "es" | "en">
       <body>
         {children}
         <WhatsAppFloat />
